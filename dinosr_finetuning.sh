df0bd60575c49cff50d559505177da310ef1eba1
ShearmanChua <shearman_chua@hotmail.com>
2024-11-08 17:12:26 +0800

    refactor: change config for dinosr finetuning

--- dinosr_finetuning.sh
+++ dinosr_finetuning.sh
@@ -1,11 +1,17 @@
-ROOT_MODEL_DIR=/models/dinosr/librispeech_960_bs_10m/finetuned_librispeech_100_bs_6.4m
+# ROOT_MODEL_DIR=/models/dinosr/librispeech_960_bs_10m
+# PRETRAINED_MODEL_DIR=${ROOT_MODEL_DIR}/ckpt/checkpoint_72_400000.pt
+# FINETUNED_MODEL_DIR=${ROOT_MODEL_DIR}/finetuned_librispeech_100_bs_6.4m
 
-mkdir -p $ROOT_MODEL_DIR
+ROOT_MODEL_DIR=/models/dinosr/librispeech_default_pretrained
+PRETRAINED_MODEL_DIR=${ROOT_MODEL_DIR}/dinosr.ckpt
+FINETUNED_MODEL_DIR=${ROOT_MODEL_DIR}/finetuned_librispeech_100_bs_6.4m
+
+mkdir -p $FINETUNED_MODEL_DIR
 python3 fairseq_cli/hydra_train.py -m \
     --config-dir examples/wav2vec/config/finetuning \
     --config-name base_100h \
-    checkpoint.root_model_dir=$ROOT_MODEL_DIR \
+    checkpoint.root_model_dir=$FINETUNED_MODEL_DIR \
     common.user_dir=examples/dinosr \
     task.data=/datasets/librispeech/finetuning_config  \
-    model.w2v_path=/models/dinosr/librispeech_960_bs_10m/ckpt/checkpoint_72_400000.pt \
+    model.w2v_path=$PRETRAINED_MODEL_DIR \
     task.normalize=True
