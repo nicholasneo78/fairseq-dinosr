b917f3a45d07bce6d3a6bbce6b9d81ae60a4d86e
nicholasneo78 <nicholasneo78@gmail.com>
2024-12-22 22:51:10 +0800

    refactor: refactor bash script and config file for pretraining model to save every N interval steps instead of just saving the latest interval

--- dinosr_pretraining.sh
+++ dinosr_pretraining.sh
@@ -1,4 +1,5 @@
-ROOT_MODEL_DIR=/models/dinosr/librispeech_960_bs_10m_steps_2.4m
+ROOT_MODEL_DIR=/models/dinosr/librispeech_960_bs_10m_steps_2.4m_retrained
+# ROOT_MODEL_DIR=/models/dinosr/librispeech_test
 
 mkdir -p $ROOT_MODEL_DIR
 python3 fairseq_cli/hydra_train.py -m \
@@ -7,4 +8,3 @@ python3 fairseq_cli/hydra_train.py -m \
     checkpoint.root_model_dir=$ROOT_MODEL_DIR \
     task.data=/datasets/librispeech/ \
     common.user_dir=examples/dinosr &
-    
